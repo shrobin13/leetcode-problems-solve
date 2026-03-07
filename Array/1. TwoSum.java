32481d2cd43ee66d9e17e5767648027e1c69b46f
Sheikh Robin <145149707+shrobin13@users.noreply.github.com>
2026-03-07 23:21:38 +0600

    just numbered them

--- Array/1. TwoSum.java
+++ Array/1. TwoSum.java
@@ -4,19 +4,27 @@ import java.util.Map;
 
 class TwoSum {
   public int[] twoSum(int[] nums, int target) {
+
     Map<Integer, Integer> isTwoSum = new HashMap<>();
+
     for (int i = 0; i < nums.length; i++) {
+
       int complement = target - nums[i];
+
       if (isTwoSum.containsKey(complement))
         return new int[] { isTwoSum.get(complement), i };
+
       isTwoSum.put(nums[i], i);
     }
+
     return new int[] { -1, -1 };
   }
 
   public static void main(String[] args) {
+
     TwoSum ts = new TwoSum();
     int[] arr = { 2, 7, 11, 15 };
+
     System.out.println(Arrays.toString(ts.twoSum(arr, 9)));
   }
 }
